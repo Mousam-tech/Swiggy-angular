8c3741c2b00226b895dcb035709ab88e1f38ef38
Mousam-tech <mousam36.work@gmail.com>
2025-11-20 23:41:21 +0530

    added code for restaurant menu items

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { AboutComponent } from './component/about/about.component';
 import { HelpComponent } from './component/help/help.component';
 import { OffersComponent } from './component/offers/offers.component';
 import { CartComponent } from './component/cart/cart.component';
+import { RestaurantMenuComponent } from './component/restaurant-menu/restaurant-menu.component';
+import { RestaurauntFilterFoodComponent } from './component/restauraunt-filter-food/restauraunt-filter-food.component';
 import { SearchComponent } from './component/search/search.component';
 
 export const routes: Routes = [
@@ -13,5 +15,7 @@ export const routes: Routes = [
     {path:"help",component:HelpComponent},
     {path:"offers",component:OffersComponent},
     {path:"cart",component:CartComponent},
+    {path:"restromenu/:id",component:RestaurantMenuComponent},
+    {path:"restaurauntFilterFood/:cuisine",component:RestaurauntFilterFoodComponent},
     {path:"search",component:SearchComponent}
 ];
